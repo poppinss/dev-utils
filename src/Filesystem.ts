f97f8772b89e6c8d0d79a2044b8dc6a6db3eb91f
Harminder virk <virk.officials@gmail.com>
2020-07-03 19:01:32 +0530

    chore: add prettier

--- src/Filesystem.ts
+++ src/Filesystem.ts
@@ -28,121 +28,120 @@ import { join, extname, isAbsolute } from 'path'
  * ```
  */
 export class Filesystem {
-  private modules: Set<string> = new Set()
+	private modules: Set<string> = new Set()
 
-  /**
-   * Reference to fsExtra
-   */
-  public fsExtra = fsExtra
+	/**
+	 * Reference to fsExtra
+	 */
+	public fsExtra = fsExtra
 
-  constructor (public basePath = join(tmpdir(), `${new Date().getTime()}`)) {
-  }
+	constructor(public basePath = join(tmpdir(), `${new Date().getTime()}`)) {}
 
-  /**
-   * Returns a boolean telling if file extension is part
-   * of a Node.js module
-   */
-  private isModule (filePath: string): boolean {
-    return ['.js', '.ts', '.json'].includes(extname(filePath))
-  }
+	/**
+	 * Returns a boolean telling if file extension is part
+	 * of a Node.js module
+	 */
+	private isModule(filePath: string): boolean {
+		return ['.js', '.ts', '.json'].includes(extname(filePath))
+	}
 
-  /**
-   * Makes abs path to a given file
-   */
-  private makePath (filePath: string): string {
-    return isAbsolute(filePath) ? filePath : join(this.basePath, filePath)
-  }
+	/**
+	 * Makes abs path to a given file
+	 */
+	private makePath(filePath: string): string {
+		return isAbsolute(filePath) ? filePath : join(this.basePath, filePath)
+	}
 
-  /**
-   * Removes ext from the file path
-   */
-  private dropExt (filePath: string): string {
-    return filePath.replace(/\.\w+$/, '')
-  }
+	/**
+	 * Removes ext from the file path
+	 */
+	private dropExt(filePath: string): string {
+		return filePath.replace(/\.\w+$/, '')
+	}
 
-  /**
-   * Removes the file path from nodejs module cache
-   */
-  private removeFromModule (filePath: string): void {
-    const absPath = this.makePath(filePath)
-    this.modules.delete(absPath)
+	/**
+	 * Removes the file path from nodejs module cache
+	 */
+	private removeFromModule(filePath: string): void {
+		const absPath = this.makePath(filePath)
+		this.modules.delete(absPath)
 
-    /**
-     * Clear module raises error if file is not
-     * in require cache, we can safely ignore
-     * the error
-     */
-    try {
-      clearModule(absPath)
-    } catch (error) {}
-  }
+		/**
+		 * Clear module raises error if file is not
+		 * in require cache, we can safely ignore
+		 * the error
+		 */
+		try {
+			clearModule(absPath)
+		} catch (error) {}
+	}
 
-  /**
-   * Store reference of a given file to clear it from the
-   * modules cache at a later stage
-   */
-  private addToModule (filePath: string): void {
-    if (!this.isModule(filePath)) {
-      return
-    }
+	/**
+	 * Store reference of a given file to clear it from the
+	 * modules cache at a later stage
+	 */
+	private addToModule(filePath: string): void {
+		if (!this.isModule(filePath)) {
+			return
+		}
 
-    this.modules.add(this.makePath(filePath))
-  }
+		this.modules.add(this.makePath(filePath))
+	}
 
-  /**
-   * Add a new file with given contents
-   */
-  public async add (filePath: string, contents: string): Promise<void> {
-    const absPath = this.makePath(filePath)
-    await this.fsExtra.outputFile(absPath, contents)
+	/**
+	 * Add a new file with given contents
+	 */
+	public async add(filePath: string, contents: string): Promise<void> {
+		const absPath = this.makePath(filePath)
+		await this.fsExtra.outputFile(absPath, contents)
 
-    this.addToModule(filePath)
-  }
+		this.addToModule(filePath)
+	}
 
-  /**
-   * Returns true when file exists on the disk
-   */
-  public async exists (filePath: string): Promise<boolean> {
-    return this.fsExtra.pathExists(this.makePath(filePath))
-  }
+	/**
+	 * Returns true when file exists on the disk
+	 */
+	public async exists(filePath: string): Promise<boolean> {
+		return this.fsExtra.pathExists(this.makePath(filePath))
+	}
 
-  /**
-   * Creates base path dir (if missing)
-   */
-  public async ensureRoot (): Promise<void> {
-    return this.fsExtra.ensureDir(this.basePath)
-  }
+	/**
+	 * Creates base path dir (if missing)
+	 */
+	public async ensureRoot(): Promise<void> {
+		return this.fsExtra.ensureDir(this.basePath)
+	}
 
-  /**
-   * Returns file contents
-   */
-  public async get (filePath: string): Promise<string> {
-    return this.fsExtra.readFile(this.makePath(filePath), 'utf-8')
-  }
+	/**
+	 * Returns file contents
+	 */
+	public async get(filePath: string): Promise<string> {
+		return this.fsExtra.readFile(this.makePath(filePath), 'utf-8')
+	}
 
-  /**
-   * Remove file
-   */
-  public async remove (filePath: string): Promise<void> {
-    const absPath = this.makePath(filePath)
-    await this.fsExtra.remove(absPath)
+	/**
+	 * Remove file
+	 */
+	public async remove(filePath: string): Promise<void> {
+		const absPath = this.makePath(filePath)
+		await this.fsExtra.remove(absPath)
 
-    const withoutExt = this.dropExt(absPath)
-    if (this.modules.has(absPath) || this.modules.has(withoutExt)) {
-      this.removeFromModule(filePath)
-      this.removeFromModule(withoutExt)
-      return
-    }
-  }
+		const withoutExt = this.dropExt(absPath)
+		if (this.modules.has(absPath) || this.modules.has(withoutExt)) {
+			this.removeFromModule(filePath)
+			this.removeFromModule(withoutExt)
+			return
+		}
+	}
 
-  /**
-   * Cleanup all files and modules cache (if any)
-   */
-  public async cleanup (): Promise<void> {
-    await this.fsExtra.remove(this.basePath)
-    this.modules.forEach((mod) => {
-      this.removeFromModule(mod)
-      this.removeFromModule(this.dropExt(mod))
-    })
-  }
+	/**
+	 * Cleanup all files and modules cache (if any)
+	 */
+	public async cleanup(): Promise<void> {
+		await this.fsExtra.remove(this.basePath)
+		this.modules.forEach((mod) => {
+			this.removeFromModule(mod)
+			this.removeFromModule(this.dropExt(mod))
+		})
+	}
 }
